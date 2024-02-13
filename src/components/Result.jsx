23572f435a71802065cf2b3e066c8223198798ca
danie odukwe <kingdanny295@gmail.com>
2024-02-13 20:12:21 +0100

    made it more readable

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,10 +4,7 @@ import axios from 'axios';
 
 const Result = () => {
   const [search, setSearch] = useState('');
-  const [result, setResult] = useState('');
-  const [ word, setWord ] = useState('')
-  const [ author, setAuthor ] = useState('')
-  const [ example, setExample ] = useState ('')
+  const [apiData, setApiData] = useState(null);
   const [error, setError] = useState(null);
   const [buttonClicked, setButtonClicked] = useState(false);
 
@@ -20,11 +17,7 @@ const Result = () => {
           'X-RapidAPI-Host': 'urban-dictionary7.p.rapidapi.com',
         },
       });
-      setResult(res.data.list[0].definition);
-      setAuthor(res.data.list[0].author)
-      setExample(res.data.list[0].example)
-      setWord(res.data.list[0].word)
-      console.log(result);
+      setApiData(res.data.list[0]);
     } catch (error) {
       setError(error);
     }
@@ -33,9 +26,15 @@ const Result = () => {
   useEffect(() => {
     if (buttonClicked) {
       fetchData();
-      setButtonClicked(false); // Reset the buttonClicked state after making the API call
+      setButtonClicked(false);
     }
   }, [buttonClicked]);
+
+  // Accessing individual pieces of data
+  const word = apiData?.word || '';
+  const author = apiData?.author || '';
+  const example = apiData?.example || '';
+  const result = apiData?.definition || '';
 
   return (
     <div className="mt-[30px] w-full flex flex-col items-center">
@@ -49,10 +48,7 @@ const Result = () => {
         <div className="absolute right-[10px] top-[10px]">
           <FcSearch
             className="text-[1.3rem]"
-            onClick={() => {
-              // Set the buttonClicked state to true when the button is clicked
-              setButtonClicked(true);
-            }}
+            onClick={() => setButtonClicked(true)}
           />
         </div>
       </div>
@@ -61,15 +57,14 @@ const Result = () => {
         {author && (<span className='text-[0.8rem]'>author: {author}</span>)}
       </div>
       <div className='italic text-[0.9rem]'>
-       { example && (<span> example: {example}</span>)}
+        {example && (<span> example: {example}</span>)}
       </div>
-      { error ? "oops something went wrong" 
-        :
+      {error ? "oops something went wrong" :
         result && (
-        <div className='bg-gray-400 text-white mt-[10px] p-3 leading-[1.5rem] rounded-[15px] w-full min-h-[180px]'>
-          <p className='mt-[15px]'>{result}</p>
-        </div>
-      )}
+          <div className='bg-gray-400 text-white mt-[10px] p-3 leading-[1.5rem] rounded-[15px] w-full min-h-[180px]'>
+            <p className='mt-[15px]'>{result}</p>
+          </div>
+        )}
     </div>
   );
 };
