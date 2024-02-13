9e548514e15c88f77637ec26c7006f0bbafc30c8
danie odukwe <kingdanny295@gmail.com>
2024-02-13 19:14:06 +0100

    works perfectly

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import Result from "./components/Result"
 
 const App = () => {
   return (
-    <div className="max-w-[1440px] m-auto p-3 bg-slate-300">
-      <Hero />
-      <Result />
+    <div className="bg-slate-300 min-h-[100vh]">
+      <div className="max-w-[1440px] m-auto p-3">
+        <Hero />
+        <Result />
+      </div>
     </div>
   )
 }
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,45 +1,77 @@
-import { useState,useEffect } from 'react'
-import { FcSearch } from "react-icons/fc";
-import axios from 'axios'
+import { useState, useEffect } from 'react';
+import { FcSearch } from 'react-icons/fc';
+import axios from 'axios';
 
 const Result = () => {
-    const [ search, setSearch ] = useState("")
-    const [ error, setError ] = useState(null)
+  const [search, setSearch] = useState('');
+  const [result, setResult] = useState('');
+  const [ word, setWord ] = useState('')
+  const [ author, setAuthor ] = useState('')
+  const [ example, setExample ] = useState ('')
+  const [error, setError] = useState(null);
+  const [buttonClicked, setButtonClicked] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await axios.get('https://urban-dictionary7.p.rapidapi.com/v0/define',{
-                    params: {term: 'yeet'},
-                    headers: {
-                        'X-RapidAPI-Key': '0f6be86a06mshc96bafaac64025fp1fe08cjsn42fd3ef9c55d',
-                        'X-RapidAPI-Host': 'urban-dictionary7.p.rapidapi.com'
-                    }
-                })
-                setSearch(res.data.list[0].definition)
-                console.log(search)
-            } catch (error) {
-                setError(error)
-            }
-        }
-        fetchData()
-    },[])
+  const fetchData = async () => {
+    try {
+      const res = await axios.get('https://urban-dictionary7.p.rapidapi.com/v0/define', {
+        params: { term: `${search}` },
+        headers: {
+          'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+          'X-RapidAPI-Host': 'urban-dictionary7.p.rapidapi.com',
+        },
+      });
+      setResult(res.data.list[0].definition);
+      setAuthor(res.data.list[0].author)
+      setExample(res.data.list[0].example)
+      setWord(res.data.list[0].word)
+      console.log(result);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    if (buttonClicked) {
+      fetchData();
+      setButtonClicked(false); // Reset the buttonClicked state after making the API call
+    }
+  }, [buttonClicked]);
 
   return (
     <div className="mt-[30px] w-full flex flex-col items-center">
-        <div className="relative">
-            <input 
-                type="text"
-                placeholder="enter a word.."
-                className="py-3 px-6 w-full min-w-[300px] text-[0.8rem] outline-none rounded-[90px]"
-                onChange={(e)=> setSearch(e.target.value)}
-            />
-            <div className="absolute right-[10px] top-[10px]">
-                <FcSearch className="text-[1.3rem]"/>
-            </div>
+      <div className="relative">
+        <input
+          type="text"
+          placeholder="enter a word.."
+          className="py-3 px-6 w-full min-w-[300px] text-[0.8rem] outline-none rounded-[90px] bg-gray-400 text-white"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <div className="absolute right-[10px] top-[10px]">
+          <FcSearch
+            className="text-[1.3rem]"
+            onClick={() => {
+              // Set the buttonClicked state to true when the button is clicked
+              setButtonClicked(true);
+            }}
+          />
         </div>
+      </div>
+      <div className='w-full my-[15px] flex justify-evenly items-end uppercase text-[1.5rem] font-semibold text-gray-600'>
+        <span className='text-black'>{word}</span>
+        <span className='text-[0.8rem]'>author: {author}</span>
+      </div>
+      <div className='italic text-[0.9rem]'>
+        example: {example}
+      </div>
+      { error ? "oops something went wrong" 
+        :
+        result && (
+        <div className='bg-gray-400 text-white mt-[10px] p-3 leading-[1.5rem] rounded-[15px] w-full min-h-[180px]'>
+          <p className='mt-[15px]'>{result}</p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Result
+export default Result;
