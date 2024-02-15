e43964250016b14ccf6ebce667c1a47386888662
danie odukwe <kingdanny295@gmail.com>
2024-02-15 12:44:42 +0100

    added a search history

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,23 +1,31 @@
 import { useState, useEffect } from 'react';
 import { FcSearch } from 'react-icons/fc';
 import axios from 'axios';
+import copy from '../assets/copy.svg'
+import tick from '../assets/tick.svg'
 
 const Result = () => {
-  const [search, setSearch] = useState('');
-  const [apiData, setApiData] = useState([]);
-  const [error, setError] = useState(null);
-  const [buttonClicked, setButtonClicked] = useState(false);
+  const [search, setSearch] = useState(''); // setting the initla state of the input form
+  const [apiData, setApiData] = useState([]); // an array that stores all data from the api
+  const [error, setError] = useState(null); // checks for erros and displays it
+  const [buttonClicked, setButtonClicked] = useState(false); // an optional button for the search input to make the fetchData call when clicked
+  const [history, setHistory] = useState([]) //an array that stores search history
+  const [copied, setCopied] = useState('')
 
   const fetchData = async () => {
     try {
       const res = await axios.get('https://urban-dictionary7.p.rapidapi.com/v0/define', {
         params: { term: `${search}` },
         headers: {
-          'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+          'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY, // api key from rapid api stored in .env file
           'X-RapidAPI-Host': 'urban-dictionary7.p.rapidapi.com',
         },
       });
         setApiData(res.data.list);
+        //update search history
+        const updatedHistory = [...history, search]
+        setHistory(updatedHistory)
+        localStorage.setItem('searchHistory', JSON.stringify(updatedHistory))
       } catch (error) {
         setError(error);
       }
@@ -30,15 +38,27 @@ const Result = () => {
     }
   }, [buttonClicked]);
 
+  useEffect(()=> {
+    //load search history from localstorage on component mount
+    const storedHistory = localStorage.getItem('searchHistory');
+    if (storedHistory) {
+      setHistory(JSON.parse(storedHistory))
+    }
+  },[])
   // Accessing individual pieces of data
   const word = apiData[0]?.word || '';
 
   const handleSubmit = (e) => {
     e.preventDefault()
     setButtonClicked(true)
-
   }
 
+  //now to set a copy function that copies a history link when clicked
+  const handleCopy = (text) => {
+    setCopied(text)
+    navigator.clipboard.writeText(text)
+    setTimeout(()=> setCopied(false),3000)
+  }
 
   return (
     <div className="mt-[30px] w-full flex flex-col items-center">
@@ -60,7 +80,36 @@ const Result = () => {
             />
           </button>
         </form>
-      </div>
+
+        {/* Search history display */}
+        {
+          history &&( <div className="my-5">
+          <h3 className="font-semibold mb-2"> History:</h3>
+          <ul className='overflow-y-auto max-h-[100px] scrollable'>
+            {history.map((item, index) => (
+              <div
+                key={index}
+                onClick={()=> setSearch(item)}
+              >
+                <div className='flex justify-between p-2 cursor-pointer my-2 bg-white rounded-xl'>
+                  <img 
+                    src={ copied === item ? tick : copy }
+                    alt="copy_icon" 
+                    className='w-[15px] h-[15px] object-contain'
+                    onClick={()=> handleCopy(item)}
+                  />
+                  <p className='text-[0.9rem] text-gray-600 capitalize'>
+                    {item}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </ul>
+          </div>)
+        }
+
+        </div>
+
         <span className='text-[1.5rem] uppercase font-semibold text-gray-600 mt-[30px]'>{word}</span>
         <div className='flex overflow-x-auto min-w-[380px] w-full p-3 mt-[20px]'>
           {
